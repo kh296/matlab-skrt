5ef79dbb1a21035607a1f69d871c169929dd2d03
K. Harrison <kh296@users.noreply.github.com>
2023-07-13 17:41:20 +0100

    Allow for registration of images with different data types

--- +mskrt/niftisave.m
+++ +mskrt/niftisave.m
@@ -8,11 +8,21 @@ function [] = niftisave(imageVoxels, info, outPath)
     %     The voxel array is adjusted prior to writing, to reverse
     %     the modifications of niftiload().
     
+    % Obtain auto-generated metadata
+    % for voxel data written in NIfTI format.
+    niftiwrite(imageVoxels, outPath);
+    info2 = niftiinfo(outPath);
+
+    % Modify input metadata, 
+    % based on size and type of imageVoxels array.
     ndim = ndims(imageVoxels);
     imageVoxels = permute(flip(imageVoxels, 1), [2, 1, 3:ndim]);
     info.ImageSize = size(imageVoxels);
     nextra = ndim - numel(info.PixelDimensions);
-    info.PixelDimensions = [info.PixelDimensions, ones(1, nextra)];
+    info.PixelDimensions = [info.PixelDimensions, ones(1, nextra)];    
+    info.Datatype = info2.Datatype;
+    info.BitsPerPixel = info2.BitsPerPixel;
+
     niftiwrite(imageVoxels, outPath, info);
 end
 
